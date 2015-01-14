568fe4776ff1024e1b92764f3dba2666a6196393
Daniel Romero <danielromeroauk@gmail.com>
2015-01-14 01:23:32 -0500

    Menú de tercero organizado en tres grupos: tercero, carteras por pagar y carteras por cobrar.

--- app/views/partials/tercero_menu.blade.php
+++ app/views/partials/tercero_menu.blade.php
@@ -8,16 +8,26 @@
 	<a href="{{route('crear_cuenta', ['id_tercero' => $tercero->id])}}" class="list-group-item">
 		Crear cuenta bancaria
 	</a>
-	<a href="{{route('crear_cartera', ['documento' => 'PAGAR', 'tercero' => $tercero->id])}}" class="list-group-item">
-		Crear cartera por pagar
+
+	<a class="list-group-item active" href="{{route('listado_de_carteras', ['documento' => 'PAGAR',
+	'tercero' => $tercero->id])}}">
+		Carteras por pagar
 	</a>
-	<a href="{{route('crear_cartera', ['documento' => 'COBRAR', 'tercero' => $tercero->id])}}" class="list-group-item">
-		Crear cartera por cobrar
+	<a href="{{route('crear_cartera', ['documento' => 'PAGAR', 'tercero' => $tercero->id])}}" class="list-group-item">
+		Crear cartera por <strong>pagar</strong>
 	</a>
 	<a href="{{route('listado_de_carteras', ['documento' => 'PAGAR', 'tercero' => $tercero->id])}}" class="list-group-item">
-		Ver carteras por pagar de este tercero
+		Ver carteras por <strong>pagar</strong> de este tercero
+	</a>
+
+	<a class="list-group-item active" href="{{route('listado_de_carteras', ['documento' => 'COBRAR',
+	'tercero' => $tercero->id])}}">
+		Carteras por cobrar
+	</a>
+	<a href="{{route('crear_cartera', ['documento' => 'COBRAR', 'tercero' => $tercero->id])}}" class="list-group-item">
+		Crear cartera por <strong>cobrar</strong>
 	</a>
 	<a href="{{route('listado_de_carteras', ['documento' => 'COBRAR', 'tercero' => $tercero->id])}}" class="list-group-item">
-		Ver carteras por cobrar de este tercero
+		Ver carteras por <strong>cobrar</strong> de este tercero
 	</a>
 </div>
