55682d848ed516d2e3a356c960b56475399b69ee
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2015-02-06 00:40:31 -0500

    Se agregó el meta viewport a master.blade.php

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<meta charset="utf-8">
-<title>
-	@section('title') Cartera @show
-</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>
+		@section('title') Cartera @show
+	</title>
 
 {{HTML::style('bootstrap/css/bootstrap-united.min.css')}}
 {{HTML::style('css/main.css')}}
